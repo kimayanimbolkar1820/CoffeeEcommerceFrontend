5eb9bd6d4b6707f6125b7d815cd95effb7875a57
Kimaya <kimaya123@gmail.com>
2026-01-08 13:29:42 +0530

    conflicts resolve

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,6 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
-import { Playfair_Display } from "next/font/google";
-import { ThemeProvider } from "next-themes";
-import Footer from "@/components/Footer";
+
 import { Playfair_Display, Inter } from "next/font/google";
 
 const playfair = Playfair_Display({
@@ -21,21 +19,16 @@ const inter = Inter({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={playfair.variable} suppressHydrationWarning>
-      <body className="font-serif bg-black text-white">
-        <ThemeProvider>
-          <Navbar />
-          {children}
-          <Footer /> 
-        </ThemeProvider>
-       
+   
     <html
       lang="en"
       className={`${playfair.variable} ${inter.variable}`}
     >
       <body className="bg-black text-white font-inter">
         <Navbar />
+        
         {children}
+       
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
 import Hero from '@/components/Hero'
 import PricingCards from '@/components/PricingCards'
-// import Navbar from '@/components/Navbar'
+
 import React from 'react'
-import Hero from '@/components/Hero'
+
 import Categories from '@/components/Categories'
 import FeaturedDrinks from '@/components/FeaturedDrinks'
 
 const Page = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+     
       <Hero />
-      <PricingCards/>
-    </div>
-     <Hero/>
+    
+   
      <Categories/>
      <FeaturedDrinks/>
+     <PricingCards/>
       </div>
   )
 }
