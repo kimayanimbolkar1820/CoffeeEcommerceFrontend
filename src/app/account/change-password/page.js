51c8c33e336fe1aa54fc850db97e2322d26a9081
Payal Nikale <payalnikale143@gmail.com>
2026-02-07 16:54:21 +0530

    ProfileView.jsx or Profile.jsx changes

--- src/app/account/change-password/page.js
+++ src/app/account/change-password/page.js
@@ -5,21 +5,12 @@ import { Eye, EyeOff } from "lucide-react";
 import { motion } from "framer-motion";
 
 export default function ChangePassword({ onBack }) {
-=======
-
-import { useState } from "react";
-import { Eye, EyeOff } from "lucide-react";
-import { motion } from "framer-motion";
-
-export default function ChangePassword() {
   const [showCurrent, setShowCurrent] = useState(false);
   const [showNew, setShowNew] = useState(false);
   const [showConfirm, setShowConfirm] = useState(false);
 
   return (
     <div className="min-h-screen flex justify-center items-start bg-[#d7bf9a] px-4 sm:px-6 py-6">
-    <div className="flex justify-center px-4 sm:px-6">
-
       <motion.div
         initial={{ opacity: 0, y: 30 }}
         animate={{ opacity: 1, y: 0 }}
@@ -51,11 +42,9 @@ export default function ChangePassword() {
           </motion.button>
         </motion.div>
 
-        <h2 className="title text-center mb-20 mt-8 underline underline-offset-2 decoration-1 text-[rgb(228,185,154)] font-semibold text-2xl sm:text-3xl">
+        <h2 className="title text-center mb-6 mt-8 text-[rgb(228,185,154)] font-semibold text-2xl sm:text-3xl">
           Change Password
         </h2>
-       
-        <h2 className="title text-center mb-6">Change Password</h2>
 
         <div className="space-y-4">
           {/* Current Password */}
@@ -126,29 +115,6 @@ export default function ChangePassword() {
           border-radius: 0.5rem;
         }
       `}</style>
-        {/* Animated Button */}
- <div className="flex justify-center ">
-  <motion.button
-    whileTap={{ scale: 0.94 }}
-    whileHover={{ scale: 1.03 }}
-    transition={{ type: "spring", stiffness: 260 }}
-    onClick={() => {
-      console.log("Update Password clicked");
-      // call API here
-    }}
-    className="
-      btn-primary btn-glow 
-      w-60 py-3 mt-6
-      rounded-lg
-      text-base sm:text-lg
-      flex items-center justify-center
-    "
-  >
-    Update Password
-  </motion.button>
-</div>
-
-      </motion.div>
     </div>
   );
 }
--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -1,15 +1,16 @@
 "use client";
 
-// import Sidebar from "@/components/account/Sidebar";
-
-
 export default function AccountLayout({ children }) {
   return (
-    <div className="flex min-h-screen bg-[#27180e] md:bg-[#291a0d]">
-      {/* <Sidebar /> */}
-      <main className="flex-1 p-5 mb-p-2">{children}</main>
+    <div className="flex  min-h-screen gap-6">
+      {/* LEFT SIDEBAR */}
+        {/* your menu UI exactly as it is */}
 
-      <main className="flex-1 p-5 md:p-6">{children}</main>
+      {/* RIGHT CONTENT */}
+      <main className="flex-1 bg-[#2a1c12] rounded-xl">
+        {children}
+      </main>
     </div>
   );
 }
+
